dd19fe4f1083b3282b23cd288b5493a0052b1b66
unknown <ahmetsdogan16@gmail.com>
2023-11-18 11:34:03 +0300

    li etiketi oluşturdum

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-<li class="d-flex justify-content-between align-items-center bg-info px-3 rounded-2">
+{/* <li class="d-flex justify-content-between align-items-center bg-info px-3 rounded-2">
     <p class="mt-3">İnputta yazılacak değer</p>
     <div class="d-flex gap-3">
     <i class="fa-solid fa-thumbs-up fa-beat"></i>
     <i class="fa-solid fa-trash fa-bounce"></i>
     </div>
-</li> 
+</li>  */}
+
+const input = document.getElementById("input")
+const btn = document.querySelector("#btn")
+const liste = document.querySelector("#liste")
+
+
+function toDo() {
+    const li = document.createElement("li")
+    li.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'bg-warning', 'px-3', 'rounded-2')
+    console.log(li)
+
+
+
+}
